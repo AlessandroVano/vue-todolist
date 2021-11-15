eb081d1458116ca6fec6df125b2307487cc1b13e
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-15 17:31:21 +0100

    inserimento  array con 3 oggetti

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
+/**
+ * Partire dalla definizione di un array con 3 oggetti ( i todo).
+Ogni oggetto todo avrà le seguenti proprietà:
+- text, una stringa che indica il testo del todo
+- completed, un booleano (true/false) che indica se il todo è stato completato oppure no
+MILESTONE 1
+Stampare all’interno di una lista, un item per ogni todo.
+Se la proprietà completed è uguale a true, visualizzare il testo del todo sbarrato.
+MILESTONE 2
+Visualizzare a fianco di ogni todo una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+ */
+
+
+
 const app = new Vue ({
     el: '#app',
-    data: {}
+    data: {
+        todos: [
+            {
+                text: 'Dare da mangiare al cane',
+                completed: false,
+            },
+            {
+                text: 'Andare in banca',
+                completed: true,
+            },
+            {
+                text: 'cucinare per la sera',
+                completed: false,
+            },
+        ],
+    },
     
 })
